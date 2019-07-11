26e10eecd286968a71ae95756f0c1cf9cb2ec7ea
shabbeersh <36790230+shabbeersh@users.noreply.github.com>
2019-07-11 14:44:01 +0530

    Update AutoFC_AlexNet_bayesian.py

--- CalTech101/AutoFC_AlexNet_bayesian.py
+++ CalTech101/AutoFC_AlexNet_bayesian.py
@@ -8,7 +8,6 @@ from keras.applications import *
 from keras import models, layers, callbacks, activations
 from keras.backend import tf as ktf
 from keras.utils import multi_gpu_model, Sequence
-#from bayes_opt import BayesianOptimization
 from keras.utils import multi_gpu_model
 from datetime import datetime
 import pandas as pd
@@ -18,56 +17,43 @@ batch_size=8
 TRAIN_PATH = os.path.join("Caltech101", "training")
 VALID_PATH = os.path.join("Caltech101", "validation")
 NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH))
-
 # Creating generators from training and validation data
 train_datagen = image.ImageDataGenerator()
 train_generator = train_datagen.flow_from_directory(TRAIN_PATH, target_size=(224, 224), batch_size=8)
-
 valid_datagen = image.ImageDataGenerator()
 valid_generator = valid_datagen.flow_from_directory(VALID_PATH, target_size=(224, 224), batch_size=8)
-
 def get_model(num_layers, num_neurons, dropout, activation, weight_initializer):
     base_model = AlexNet()
     for layer in base_model.layers:
         layer.trainable = False
-
     X = base_model.layers[-9].output
     for i in range(num_layers):
         X = layers.Dense(num_neurons[i], activation=activation, kernel_initializer=weight_initializer)(X)
         X = layers.Dropout(dropout)(X)
         X = layers.BatchNormalization()(X)
-
     X = layers.Dense(NUMBER_OF_CLASSES, activation='softmax')(X)
     model = models.Model(inputs=base_model.inputs, outputs=X)
     return model
-
 try:
     log_df = pd.read_csv(os.path.join("AutoFC_AlexNet", "AutoFC_AlexNet_log_CalTech_101_bayes_opt_v1.csv"), header=0, index_col=['index'])
 except FileNotFoundError:
     log_df = pd.DataFrame(columns=['index', 'activation', 'weight_initializer', 'dropout', 'num_neurons', 'num_layers', 'train_loss', 'train_acc', 'val_loss', 'val_acc'])
     log_df = log_df.set_index('index')
-
 print("Shape:", log_df.shape)
 early_callback = callbacks.EarlyStopping(monitor="val_acc", patience=5, mode="auto")
-
 from itertools import product
-
 p_space = {
     'activation': ['relu', 'tanh', 'sigmoid'],
     'weight_initializer': ['he_normal'],
     'num_layers': list(range(0,3))
     #'weight_initializer': ['constant', 'normal', 'uniform', 'glorot_uniform', 'glorot_normal', 'he_normal', 'he_uniform', 'orthogonal']
 }
-
 p_space = list(product(*p_space.values()))
-
 start = datetime.time(datetime.now())
 print("Starting:", start)
-
 for combo in p_space:
     print(combo)
     activation, weight_initializer, num_layers = combo
-
     bounds = [
         {'name': 'dropout', 'type': 'discrete', 'domain': numpy.arange(0, 0.6, 0.1)}
         #{'name': 'num_neurons', 'type': 'discrete', 'domain': [2 ** j for j in range(6, 11)]},
@@ -75,22 +61,17 @@ for combo in p_space:
         #{'name': 'activation', 'type': 'discrete', 'domain': ['relu', 'tanh', 'sigmoid']},
         #{'name': 'weight_initializer', 'type': 'discrete', 'domain': ['constant', 'normal', 'uniform', 'glorot_uniform', 'glorot_normal', 'he_normal', 'he_uniform', 'orthogonal']}
     ]
-
     for _ in range(num_layers):
         bounds.append({'name': 'num_neurons' + str(num_layers + 1), 'type': 'discrete', 'domain': [2 ** j for j in range(6, 11)]})
-
     history = None
     neurons = None
-
     def model_fit(x):
         global neurons
-        neurons = tuple(map(int, [x[:, i] for i in range(1, len(bounds) - 1)]))
-
+        neurons = tuple(map(int, [x[:, i] for i in range(1, len(bounds))]))
         print("Current Parameters:")
         print("\t{}:\t{}".format(bounds[0]['name'], x[:, 0]))
         for i in range(num_layers):
             print("\t{}:\t{}".format(bounds[i + 1]['name'], x[:, i + 1]))
-
         model = get_model(
             dropout=float(x[:, 0]),
             num_layers=num_layers,
@@ -100,17 +81,12 @@ for combo in p_space:
         )
         model = multi_gpu_model(model, gpus=2)
         model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=['accuracy'])
-
         global history
-
         history = model.fit_generator(train_generator, validation_data=valid_generator, epochs=2, callbacks=[early_callback],steps_per_epoch=len(train_generator)/batch_size, validation_steps =len(valid_generator))
         #score = model.evaluate_generator(valid_generator, verbose=1)
         return min(history.history['val_loss'])
-
-
     opt_ = GPyOpt.methods.BayesianOptimization(f=model_fit, domain=bounds)
     opt_.run_optimization(max_iter=5)
-
     # print("""
     # Optimized Parameters:
     # \t{0}:\t{1}
@@ -123,22 +99,18 @@ for combo in p_space:
     #            #bounds[4]["name"],opt_.x_opt[4],
     #            #bounds[5]["name"],opt_.x_opt[5]
     # ))
-
     print("Optimized Parameters:")
     print("\t{}:\t{}".format(bounds[0]['name'], opt_.x_opt[0]))
     for i in range(num_layers):
         print("\t{}:\t{}".format(bounds[i + 1]['name'], opt_.x_opt[i + 1]))
-
     print("optimized loss: {0}".format(opt_.fx_opt))
-    best_acc_index = history.history['val_loss'].index(min(history.history['val_loss']))
+    best_acc_index = history.history['val_acc'].index(max(history.history['val_acc']))
     log_tuple = (activation, weight_initializer, opt_.x_opt[0], neurons, num_layers, history.history['loss'][best_acc_index], history.history['acc'][best_acc_index], opt_.fx_opt, history.history['val_acc'][best_acc_index])
-    print("Activation weight_initializer dropout_rate #neurons #FClayers train_loss train_acc val_loss val_acc")
+    #print("Activation weight_initializer dropout_rate #neurons #FClayers train_loss train_acc val_loss val_acc")
     print("Logging record:", log_tuple)
     print('lof_df shape',log_df.shape[0])
     log_df.loc[log_df.shape[0]] = log_tuple
     print("Shape:", log_df.shape)
-
     log_df.to_csv(os.path.join("AutoFC_AlexNet", "AutoFC_AlexNet_log_CalTech_101_bayes_opt_v1.csv"))
-
 end = datetime.time(datetime.now())
 print("Ending:", end)
