8b260445538eb39b548c7c7f31e0d060e089d448
shabbeersh <36790230+shabbeersh@users.noreply.github.com>
2019-07-28 19:02:46 +0530

    Update AutoFC_MobileNet_Bayesian.py

--- Oxford102/AutoFC_MobileNet_Bayesian.py
+++ Oxford102/AutoFC_MobileNet_Bayesian.py
@@ -2,7 +2,8 @@ import os
 import numpy
 import matplotlib.pyplot as plt
 import random
-
+import keras
+import numpy as np
 from PIL import Image
 from keras.preprocessing import image
 from keras.applications import MobileNet
@@ -48,7 +49,7 @@ def get_model(num_layers, num_neurons, dropout, activation, weight_initializer):
     return model
 
 try:
-    log_df = pd.read_csv(os.path.join("AutoFC_MobileNet", "AutoFC_MobileNet_log_CalTech_101_bayes_opt_v1.csv"), header=0, index_col=['index'])
+    log_df = pd.read_csv(os.path.join("AutoFC_MobileNet", "AutoFC_MobileNet_log_Oxford102_bayes_opt_v1.csv"), header=0, index_col=['index'])
 except FileNotFoundError:
     log_df = pd.DataFrame(columns=['index', 'activation', 'weight_initializer', 'dropout', 'num_neurons', 'num_layers', 'train_loss', 'train_acc', 'val_loss', 'val_acc'])
     log_df = log_df.set_index('index')
@@ -132,7 +133,7 @@ for combo in p_space:
     log_df.loc[log_df.shape[0]] = log_tuple
     print("Shape:", log_df.shape)
 
-    log_df.to_csv(os.path.join("AutoFC_MobileNet", "AutoFC_MobileNet_log_CalTech_101_bayes_opt_v1.csv"))
+    log_df.to_csv(os.path.join("AutoFC_MobileNet", "AutoFC_MobileNet_log_Oxford102_bayes_opt_v1.csv"))
 
 end = datetime.time(datetime.now())
 print("Ending:", end)
