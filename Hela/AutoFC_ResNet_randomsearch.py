9d7ff5678964ebe25d0b18ba785238c090e59b1f
shabbeersh <36790230+shabbeersh@users.noreply.github.com>
2019-08-22 05:14:34 +0530

    Update AutoFC_ResNet_randomsearch.py

--- Hela/AutoFC_ResNet_randomsearch.py
+++ Hela/AutoFC_ResNet_randomsearch.py
@@ -131,7 +131,7 @@ for i in num_layers:
         X = layers.Dense(NUMBER_OF_CLASSES, activation="softmax")(X)
 
         new_model = models.Model(inputs=base_model.input, outputs=X)
-        new_model = multi_gpu_model(new_model, gpus=2)
+        #new_model = multi_gpu_model(new_model, gpus=2)
         new_model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=["accuracy"])
         start = time.time()
         history = new_model.fit_generator(train_generator, validation_data=valid_generator, epochs=20, callbacks=[lr_reducer],steps_per_epoch=len(train_generator)/batch_size, validation_steps =len(valid_generator))
