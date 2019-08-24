473241a1d2adafe9ee60e69d827e9dd2794c9fcf
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2019-08-24 15:29:33 +0530

    Fixed bug

--- Oxford102/TSNE_Features/With_ResNet50.py
+++ Oxford102/TSNE_Features/With_ResNet50.py
@@ -11,13 +11,13 @@ SELECTED_IMAGES = []
 SELECTED_IMAGES_LABELS = []
 DATA_FOLDER = "Oxford102Flowers"
 TRAIN_DIR = "training"
-FULL_DIR = os.abspath(os.path.join(DATA_FOLDER, TRAIN_DIR))
+FULL_DIR = os.path.abspath(os.path.join(DATA_FOLDER, TRAIN_DIR))
 RANDOM_CLASSES = np.random.choice(os.listdir(FULL_DIR), size=4, replace=False)
 
 for _class in RANDOM_CLASSES:
     CLASS_DIR = os.path.join(FULL_DIR, _class)
     IMAGES = np.random.choice(os.listdir(CLASS_DIR), size=15, replace=False)
-    SELECTED_IMAGES.append(IMAGES)
+    SELECTED_IMAGES.extend(IMAGES)
     SELECTED_IMAGES_LABELS.extend([int(_class)] * 15)
 # selecting random images ends here
 
@@ -30,7 +30,7 @@ X = layers.Dense(102)(X)
 model = models.Model(inputs=base_model.inputs, outputs=X)
 
 for img_path in SELECTED_IMAGES:
-    img = image.load_image(img_path, target_size=(224, 224))
+    img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
