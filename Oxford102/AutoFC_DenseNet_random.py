562c217e1b64ff856ff3c18e5fc70448b8804a85
JekyllAndHyde8999 <sravanvinakota@gmail.com>
2019-09-04 22:13:47 +0530

    Documented code

--- Oxford102/AutoFC_DenseNet_random.py
+++ Oxford102/AutoFC_DenseNet_random.py
@@ -1,160 +1,116 @@
 import os
 import numpy
-import matplotlib.pyplot as plt
 import random
 import keras
-import numpy as np
-from PIL import Image
-from keras.callbacks import ReduceLROnPlateau
-from keras.preprocessing import image
-from keras.applications import DenseNet121
-from keras import models, layers, callbacks, activations
-from keras.backend import tf as ktf
-#import keras.utils.Sequence
-from keras.utils import multi_gpu_model
-
-DATA_FOLDER = "Oxford102Flowers"
-
-train_images = []
-train_images_labels = []
-TRAIN_PATH = os.path.join(DATA_FOLDER, "training")
-VALID_PATH = os.path.join(DATA_FOLDER, "validation")
-NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH))
-
-# Creating generators from training and validation data
-batch_size = 8
-train_datagen = image.image.ImageDataGenerator(preprocessing_function=keras.applications.densenet.preprocess_input)
-train_generator = train_datagen.flow_from_directory(TRAIN_PATH, target_size=(224, 224), batch_size=batch_size)
-
-valid_datagen = image.image.ImageDataGenerator(preprocessing_function=keras.applications.densenet.preprocess_input)
-valid_generator = valid_datagen.flow_from_directory(VALID_PATH, target_size=(224, 224), batch_size=batch_size)
-
-# Freezing the ResNet50 layers
-
-#print("Hello")
-"""
-class LogEndResults(callbacks.Callback):
-    def on_train_begin(self, logs):
-        print(self.model)
-
-result_logger = LogEndResults()
-
-result_logger_2 = callbacks.LambdaCallback(on_train_end=lambda logs: print(logs))
-"""
-lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-10)
+import time
 
 import pandas as pd
 
+from itertools import product
+from keras.callbacks import ReduceLROnPlateau
+from keras.preprocessing import image
+from keras.applications import *
+from keras import models, layers
+
+# Declaring global variables
+DATA_FOLDER = "Oxford102Flowers"
+TRAIN_PATH = os.path.join(DATA_FOLDER, "training") # Path for training data
+VALID_PATH = os.path.join(DATA_FOLDER, "validation") # Path for validation data
+NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH)) # Number of classes of the dataset
+RESULTS_PATH = os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_" + DATA_FOLDER + "_random_search_v1.csv") # The path to the results file
+
+# Creating generators from training and validation data
+batch_size = 8 # the mini-batch size to use for the dataset
+datagen = image.ImageDataGenerator(preprocessing_function=keras.applications.vgg16.preprocess_input) # creating an instance of the data generator
+train_generator = datagen.flow_from_directory(TRAIN_PATH, target_size=(224, 224), batch_size=batch_size) # creating the generator for training data
+valid_generator = datagen.flow_from_directory(VALID_PATH, target_size=(224, 224), batch_size=batch_size) # creating the generator for validation data
+
+
+# Creating callbacks to use in the model
+lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-10)
+
+
+# Creating a pandas dataframe to log the results if the results file does not exist already.
 try:
-    log_df = pd.read_csv(os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_Oxford102_random_search_v1.csv"), header=0, index_col=['index'])
+    log_df = pd.read_csv(RESULTS_PATH, header=0, index_col=['index'])
 except FileNotFoundError:
     log_df = pd.DataFrame(columns=["index", "num_layers", "activation", "neurons", "dropout", "weight_initializer", "time", "train_loss", "train_acc", "val_loss", "val_acc"])
     log_df = log_df.set_index('index')
 
 
+# Creating a hyperparameter grid space to search over
 param_grid = {
-    'activation': ['relu', 'tanh', 'sigmoid'],
-    'neurons': (2  ** j for j in range(6, 11)),
-    'dropout': numpy.arange(0, 0.6, 0.1),
-    'weight_initializer': ['he_normal'],
-    'num_layers': range(0, 3)
-    #'weight_initializer': ['constant', 'normal', 'uniform', 'glorot_uniform', 'glorot_normal', 'he_normal', 'he_uniform', 'orthogonal'],
+    'activation': ['relu', 'tanh', 'sigmoid'], # the possible activation functions that can be used in each layer
+    'neurons': (2  ** j for j in range(6, 11)), # the possible number of nuerons that can be used in each layer
+    'dropout': numpy.arange(0, 0.6, 0.1), # the possible dropout values that can be used in each layer
+    'weight_initializer': ['he_normal'], # the possible weight initializer that can be used in each layer
+    'num_layers': range(0, 3) # the possible number of layers that can be used in the model
 }
 
-
-from itertools import combinations,product
-import time
-#import random
-
-
-    #temp_log_df = pd.DataFrame(list(columns=log_df.columns))
-
-    #FILE_NAME = f"{activation}{neurons}.h5"
-    #FILE_PATH = os.path.join("AutoFC_DenseNet", "saved_models", FILE_NAME)
-    #print("File name is ",FILE_NAME)
-    #print("File path is ", FILE_PATH)
-
+# removing the num_layers key from the param_grid dictionary
 num_layers = param_grid['num_layers']
 inner_grid = {key: param_grid[key] for key in param_grid.keys() if key != 'num_layers'}
-inner_hyper = list(product(*inner_grid.values()))
-#print(list(inner_hyper))
-NUM_TOTAL_PARAMS = sum([len(list(param_grid[key])) for key in param_grid])
-print(NUM_TOTAL_PARAMS)
+
+inner_hyper = list(product(*inner_grid.values())) # get combinations of the remaining hyperparameters
+
+# iterate over the number of layers
 for i in num_layers:
-    print("Hello Loop!")
-    used_seq = []
-    print("Hello 2")
-    #we_need = list(product(*[inner_hyper for _ in range(i)]))
+    temp_store = [] # store the combinations to use in this iteration
+    NUMBER_OF_SAMPLES = 1 if i == 0 else 33 # the number of configurations to be chosen randomly based on the number of layers
 
-    print("Hello 3")
-    #print("Population:", len(we_need), 20, 20 > len(we_need))
-    #in_use = random.sample(we_need, 20)
-
-    temp_store = []
-    NUMBER_OF_SAMPLES = 1 if i == 0 else 33
-    for z in range(NUMBER_OF_SAMPLES):
-        use_now = random.sample(inner_hyper, i)
-        while use_now in used_seq:
+    for z in range(NUMBER_OF_SAMPLES): # randomly sample combinations
+        use_now = random.sample(inner_hyper, i) # sample 'i' (the number of layers) configurations [one for each layer]
+        while use_now in temp_store: # ensure the sample configurations are not repeated
             use_now = random.sample(inner_hyper, i)
 
-        temp_store.append(use_now)
-    #used_seq.append(in_use)
+        temp_store.append(use_now) # store to be used in the experiment
 
-    for j in temp_store:
-        act_list = []
-        neu_list = []
-        drop_list = []
-        weight_list = []
+    for j in temp_store: # iterate over the list of sampled configurations
+        # j: list of 'i' configurations [one for each layer]
+        """a list for each type of hyperparameter to log results later on"""
+        act_list = [] # for activations
+        neu_list = [] # for number of neurons
+        drop_list = [] # for the dropout
+        weight_list = [] # for weight initializer
 
-        base_model = DenseNet121(weights="imagenet")
-        for layer in base_model.layers:
+        base_model = DenseNet121(weights="imagenet") # make an instance of an existing architecture to start the experiment
+        for layer in base_model.layers: # freeze the existing layers to make them non-trainable
             layer.trainable = False
 
-        X = base_model.layers[-2].output
+        X = base_model.layers[-2].output # removing the last fully connected layers
 
-        for k in j:
+        for k in j: # iterate over each layer's configuration
             activation = k[0]
             neurons = k[1]
             dropout = k[2]
             weight_init = k[3]
 
+            # append the hyperparameter values to the corresponding lists for logging results later on
             act_list.append(activation)
             neu_list.append(neurons)
             drop_list.append(dropout)
             weight_list.append(weight_init)
 
-
-
-            print("Model:", i, activation, neurons, dropout, weight_init)
+            # adding the fully connected layer with the current configuration
             X = layers.Dense(neurons, activation=activation, kernel_initializer=weight_init)(X)
             X = layers.Dropout(dropout)(X)
             X = layers.BatchNormalization()(X)
-        X = layers.Dense(NUMBER_OF_CLASSES, activation="softmax")(X)
+        X = layers.Dense(NUMBER_OF_CLASSES, activation="softmax")(X) # adding the final layer for classification
 
-        new_model = models.Model(inputs=base_model.input, outputs=X)
-        new_model = multi_gpu_model(new_model, gpus=2)
-        new_model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=["accuracy"])
+        new_model = models.Model(inputs=base_model.input, outputs=X) # making a keras Model instance for training
+        new_model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=["accuracy"]) # compiling the model with the specified loss function and optimizer
         start = time.time()
-        history = new_model.fit_generator(train_generator, validation_data=valid_generator, epochs=20, callbacks=[lr_reducer],steps_per_epoch=len(train_generator)/batch_size, validation_steps =len(valid_generator))
-    #print(f"Saving model {FILE_NAME}.")
-    #new_model.save(FILE_PATH)
-        time_taken = time.time() - start
-        print("Time:", time_taken)
+        history = new_model.fit_generator(train_generator,
+            validation_data=valid_generator, epochs=20, callbacks=[lr_reducer],
+            steps_per_epoch=len(train_generator)/batch_size,
+            validation_steps =len(valid_generator)
+        )
+        time_taken = time.time() - start # calculating the time taken to train the model with the current configuration
 
-    # log the reults in the log dataframe
-        best_acc_index = history.history['val_acc'].index(max(history.history['val_acc']))
+        # log the results in the log dataframe
+        best_acc_index = history.history['val_acc'].index(max(history.history['val_acc'])) # get the epoch number for the best validation accuracy
 
+        # create the log record for the current experiment
         log_tuple = (i, act_list, neu_list, drop_list, weight_list, time_taken, history.history['loss'][best_acc_index], history.history['acc'][best_acc_index], history.history['val_loss'][best_acc_index], history.history['val_acc'][best_acc_index])
-        #log_tuple = (i, act_list, neu_list, drop_list, weight_list, time_taken, loss, acc, val_loss, val_acc)
-        print("Columns:", log_df.columns)
-        print("Logging results:", log_tuple)
-        log_df.loc[log_df.shape[0]] = log_tuple
-        print(log_df.shape)
-
-        if log_df.shape[0] <= 5:
-            print(log_df.head())
-
-        print("Shape:", log_df.shape)
-
-#print(log_df.head())
-        log_df.to_csv(os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_Oxford102_random_search_v1.csv"))
+        log_df.loc[log_df.shape[0]] = log_tuple # add the record to the dataframe
+        log_df.to_csv(RESULTS_PATH) # save the dataframe in a CSV file
--- UCM_Land/AutoFC_DenseNet_Bayesian.py
+++ UCM_Land/AutoFC_DenseNet_Bayesian.py
@@ -1,142 +1,118 @@
 import os
 import numpy
-import matplotlib.pyplot as plt
-import random
 import keras
-from PIL import Image
-from keras.preprocessing import image
-from keras.applications import *
-from keras import models, layers, callbacks, activations
-from keras.backend import tf as ktf
-from keras.utils import multi_gpu_model, Sequence
-from keras.callbacks import ReduceLROnPlateau
-from keras.utils import multi_gpu_model
-from datetime import datetime
-import numpy as np
+import GPyOpt, GPy
+
 import pandas as pd
 
-import GPyOpt, GPy
-batch_size=8
-TRAIN_PATH = os.path.join("UCM_Land", "training")
-VALID_PATH = os.path.join("UCM_Land", "validation")
-NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH))
+from keras.preprocessing import image
+from keras.applications import *
+from keras import models, layers
+from keras.callbacks import ReduceLROnPlateau
+from datetime import datetime
+from itertools import product
+
+# Declaring global variables
+DATA_FOLDER = "UCM_Land"
+TRAIN_PATH = os.path.join(DATA_FOLDER, "training") # Path for training data
+VALID_PATH = os.path.join(DATA_FOLDER, "validation") # Path for validation data
+NUMBER_OF_CLASSES = len(os.listdir(TRAIN_PATH)) # Number of classes of the dataset
+RESULTS_PATH = os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_" + DATA_FOLDER + "_bayes_opt_v1.csv") # The path to the results file
 
 # Creating generators from training and validation data
-train_datagen = image.ImageDataGenerator(preprocessing_function=keras.applications.densenet.preprocess_input)
-train_generator = train_datagen.flow_from_directory(TRAIN_PATH, target_size=(224, 224), batch_size=8)
+batch_size=8 # the mini-batch size to use for the dataset
+datagen = image.ImageDataGenerator(preprocessing_function=keras.applications.vgg16.preprocess_input) # creating an instance of the data generator
+train_generator = datagen.flow_from_directory(TRAIN_PATH, target_size=(224, 224), batch_size=batch_size) # creating the generator for training data
+valid_generator = datagen.flow_from_directory(VALID_PATH, target_size=(224, 224), batch_size=batch_size) # creating the generator for validation data
 
-valid_datagen =  image.ImageDataGenerator(preprocessing_function=keras.applications.densenet.preprocess_input)
-valid_generator = valid_datagen.flow_from_directory(VALID_PATH, target_size=(224, 224), batch_size=8)
 
+# function to build a model and return it
 def get_model(num_layers, num_neurons, dropout, activation, weight_initializer):
-    base_model = DenseNet121(weights="imagenet")
-    for layer in base_model.layers:
+    base_model = DenseNet121(weights="imagenet") # make an instance of an existing architecture
+    for layer in base_model.layers: # freeze the existing layers to make them non-trainable
         layer.trainable = False
 
-    X = base_model.layers[-2].output
-    # change
+    X = base_model.layers[-2].output # removing the last fully connected layers
+
+    # add the required fully connected layers one-by-one
     for i in range(num_layers):
         X = layers.Dense(num_neurons[i], activation=activation, kernel_initializer=weight_initializer)(X)
         X = layers.Dropout(dropout[i])(X)
         X = layers.BatchNormalization()(X)
 
-    X = layers.Dense(NUMBER_OF_CLASSES, activation='softmax')(X)
-    model = models.Model(inputs=base_model.inputs, outputs=X)
-    return model
+    X = layers.Dense(NUMBER_OF_CLASSES, activation='softmax')(X) # the final layer for classification
+    model = models.Model(inputs=base_model.inputs, outputs=X) # making a keras Model instance for training
+    return model # returing the model
 
+
+# Creating callbacks to use in the model
+lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=numpy.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-10)
+
+# Creating a pandas dataframe to log the results if the results file does not exist already.
 try:
-    log_df = pd.read_csv(os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_ucm-land_bayes_opt_v1.csv"), header=0, index_col=['index'])
+    log_df = pd.read_csv(RESULTS_PATH, header=0, index_col=['index'])
 except FileNotFoundError:
     log_df = pd.DataFrame(columns=['index', 'activation', 'weight_initializer', 'dropout', 'num_neurons', 'num_layers', 'train_loss', 'train_acc', 'val_loss', 'val_acc'])
     log_df = log_df.set_index('index')
 
-print("Shape:", log_df.shape)
-lr_reducer = ReduceLROnPlateau(monitor='val_loss', factor=np.sqrt(0.1), cooldown=0, patience=5, min_lr=0.5e-10)
-from itertools import product
-p_space = {
-    'activation': ['relu', 'tanh', 'sigmoid'],
-    'weight_initializer': ['he_normal'],
-    'num_layers': list(range(0,3))
-    #'weight_initializer': ['constant', 'normal', 'uniform', 'glorot_uniform', 'glorot_normal', 'he_normal', 'he_uniform', 'orthogonal']
-}
-p_space = list(product(*p_space.values()))
-start = datetime.time(datetime.now())
-print("Starting:", start)
-for combo in p_space:
-    print(combo)
-    activation, weight_initializer, num_layers = combo
-    bounds = []
-        #{'name': 'num_neurons', 'type': 'discrete', 'domain': [2 ** j for j in range(6, 11)]},
-        #{'name': 'num_layers', 'type': 'discrete', 'domain': range(0, 4)}
-        #{'name': 'activation', 'type': 'discrete', 'domain': ['relu', 'tanh', 'sigmoid']},
-        #{'name': 'weight_initializer', 'type': 'discrete', 'domain': ['constant', 'normal', 'uniform', 'glorot_uniform', 'glorot_normal', 'he_normal', 'he_uniform', 'orthogonal']}
 
-    for i in range(num_layers):
+# the grid space of hyperparameters to search over
+p_space = {
+    'activation': ['relu', 'tanh', 'sigmoid'], # the possible activation functions that can be used in each layer
+    'weight_initializer': ['he_normal'], # the possible weight initializer that can be used in each layer
+    'num_layers': list(range(0,3)) # the possible number of layers that can be used in the model
+}
+
+p_space = list(product(*p_space.values())) # get combinations of the hyperparameters in the search space
+start = datetime.time(datetime.now())
+
+# iterate over each combination
+for combo in p_space:
+    activation, weight_initializer, num_layers = combo
+    bounds = [] # initialize a list bounds for search space for Bayesian Optimization
+    for i in range(num_layers): # add dropout ranges based on number of layers
         bounds.append({'name': 'dropout' + str(i + 1), 'type': 'discrete', 'domain': numpy.arange(0, 0.6, 0.1)})
-    for i in range(num_layers):
+    for i in range(num_layers): # add number of neurons ranges based on number of layers
         bounds.append({'name': 'num_neurons' + str(i + 1), 'type': 'discrete', 'domain': [2 ** j for j in range(6, 11)]})
 
     history = None
     neurons = None
     dropouts = None
+
+    # model_fit function: funtion to be optimized using bayesian optimization
     def model_fit(x):
-        global neurons
-        global dropouts
-        dropouts = [float(x[:, i]) for i in range(0, num_layers)]
-        neurons = [int(x[:, i]) for i in range(num_layers, len(bounds))]
-        print("Current Parameters:")
-        # for i in range(num_layers):
-        #     print("\t{}:\t{}".format(bounds[i]['name'], x[:, 0]))
-        #     print("\t{}:\t{}".format(bounds[i + 1]['name'], x[:, i + 1]))
+        global neurons # make neurons accessible outside the function
+        global dropouts # make dropouts accessible outside the function
+        dropouts = [float(x[:, i]) for i in range(0, num_layers)] # get the values of dropouts currently selected
+        neurons = [int(x[:, i]) for i in range(num_layers, len(bounds))] # get the values of num_neurons currently selected
+
         model = get_model(
             dropout=dropouts,
             num_layers=num_layers,
             num_neurons= neurons,
             activation=activation,
             weight_initializer=weight_initializer
+        ) # get the model with the current set of hyperparameters
+
+        model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=['accuracy']) # compiling the model with the specified loss function and optimizer
+
+        global history # make history accessible outside the function
+
+        history = model.fit_generator(train_generator,
+            validation_data=valid_generator, epochs=20, callbacks=[lr_reducer],
+            steps_per_epoch=len(train_generator)/batch_size,
+            validation_steps=len(valid_generator)
         )
-        model = multi_gpu_model(model, gpus=2)
-        model.compile(optimizer='adagrad', loss='categorical_crossentropy', metrics=['accuracy'])
-        global history
-        history = model.fit_generator(train_generator, validation_data=valid_generator, epochs=20, callbacks=[lr_reducer],steps_per_epoch=len(train_generator)/batch_size, validation_steps =len(valid_generator))
-        #score = model.evaluate_generator(valid_generator, verbose=1)
-        best_acc_index = history.history['val_acc'].index(max(history.history['val_acc']))
+
+        best_acc_index = history.history['val_acc'].index(max(history.history['val_acc'])) # get the epoch number for the best validation accuracy
+
+        # create the log record for the current experiment
         log_tuple = (activation, weight_initializer, dropouts, neurons, num_layers, history.history['loss'][best_acc_index], history.history['acc'][best_acc_index], history.history['val_loss'][best_acc_index], history.history['val_acc'][best_acc_index])
-        #print("Activation weight_initializer dropout_rate #neurons #FClayers train_loss train_acc val_loss val_acc")
-        print("Logging record:", log_tuple)
-        print('lof_df shape',log_df.shape[0])
-        log_df.loc[log_df.shape[0]] = log_tuple
-        return min(history.history['val_loss'])
-    opt_ = GPyOpt.methods.BayesianOptimization(f=model_fit, domain=bounds)
-    opt_.run_optimization(max_iter=5)
-    # print("""
-    # Optimized Parameters:
-    # \t{0}:\t{1}
-    # \t{2}:\t{3}
-    # \t{4}:\t{5}
-    # """.format(bounds[0]["name"],opt_.x_opt[0],
-    #            bounds[1]["name"],opt_.x_opt[1],
-    #            bounds[2]["name"],opt_.x_opt[2],
-    #            #bounds[3]["name"],opt_.x_opt[3],
-    #            #bounds[4]["name"],opt_.x_opt[4],
-    #            #bounds[5]["name"],opt_.x_opt[5]
-    # ))
-    print("Optimized Parameters:")
-    for i in range(num_layers):
-        print("\t{}:\t{}".format(bounds[i]['name'], opt_.x_opt[i]))
+        log_df.loc[log_df.shape[0]] = log_tuple # add the record to the dataframe
+        return min(history.history['val_loss']) # return value of the function
 
-    for i in range(num_layers, len(bounds)):
-        print("\t{}:\t{}".format(bounds[i]['name'], opt_.x_opt[i]))
-    print("optimized loss: {0}".format(opt_.fx_opt))
-    # best_acc_index = history.history['val_acc'].index(max(history.history['val_acc']))
-    # log_tuple = (activation, weight_initializer, dropouts, neurons, num_layers, history.history['loss'][best_acc_index], history.history['acc'][best_acc_index], opt_.fx_opt, history.history['val_acc'][best_acc_index])
-    # #print("Activation weight_initializer dropout_rate #neurons #FClayers train_loss train_acc val_loss val_acc")
-    # print("Logging record:", log_tuple)
-    # print('lof_df shape',log_df.shape[0])
-    # log_df.loc[log_df.shape[0]] = log_tuple
-    # print("Shape:", log_df.shape)
-
-    log_df.to_csv(os.path.join("AutoFC_DenseNet", "AutoFC_DenseNet_log_ucm-land_bayes_opt_v1.csv"))
+    opt_ = GPyOpt.methods.BayesianOptimization(f=model_fit, domain=bounds) # make an instance of the BayesianOptimization class with the function to optimize
+    opt_.run_optimization(max_iter=5) # run the optimization
+    log_df.to_csv(RESULTS_PATH) # save the dataframe in a CSV file
 
 end = datetime.time(datetime.now())
-print("Ending:", end)
